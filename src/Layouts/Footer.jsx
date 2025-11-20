171f3623aeb4b87f21d3d44d829fccaa85439150
apurbo <absarker238@gmail.com>
2025-11-20 23:18:15 +0600

    added brands section

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from "../components/Logo/Logo";
 
 export default function Footer() {
     return <div>
-        <footer className=" footer footer-horizontal footer-center bg-primary text-primary-content p-10">
+        <footer className=" footer footer-horizontal footer-center bg-black text-primary-content p-10">
             <aside>
                <Logo></Logo>
                 <p className="font-bold">
