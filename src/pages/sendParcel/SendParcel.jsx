10d3fb6bdb2dd5da841aaf1c9e145bd4fe7960a8
apurbo <absarker238@gmail.com>
2025-11-23 06:14:41 +0600

    added sender parcel full form completed

--- src/pages/sendParcel/SendParcel.jsx
+++ src/pages/sendParcel/SendParcel.jsx
@@ -1,25 +1,191 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useWatch } from "react-hook-form";
 import useAuth from "../../Hooks/useAuth";
 import { useLoaderData } from "react-router";
 
 export default function SendParcel() {
-    const { user } = useAuth()
-    const serviceData = useLoaderData()
-    console.log(serviceData)
-    const { register, handleSubmit, control } = useForm()
+    const { user } = useAuth();
+    const serviceCenters = useLoaderData();
 
-    // submit my sendparcel 
+    const { register, handleSubmit, control } = useForm();
+    const regionsDuplicate = serviceCenters.map(c => c.region);
+    const regions = [...new Set(regionsDuplicate)];
+
+    // real time tack region
+    const senderRegion = useWatch({ control, name: 'senderRegion' })
+    const receiverRegion = useWatch({ control, name: 'receiverRegion' })
+
+
+    const districtsByRegion = (region) => {
+        console.log(region)
+        const regionsDistrict = serviceCenters.filter(c => c.region === region);
+        const district = regionsDistrict.map(d => d.district);
+        return district;
+    };
+
     const handleSendParcel = (data) => {
-        console.log(data)
-    }
+        console.log(data);
+    };
 
-    return <div className="min-h-screen">
-        <h1 className="text-4xl text-primary font-extrabold mt-10">Send A Parcel</h1>
-        <h3 className="text-2xl text-primary font-semibold mt-5">Enter your parcel details</h3>
-        <hr className="mt-4 border-gray-400" />
-        <div>
+    return (
+        <div className="min-h-screen p-6">
+            <h1 className="text-4xl text-primary font-extrabold mt-10">Send A Parcel</h1>
+            <h3 className="text-2xl text-primary font-semibold mt-5">Enter your parcel details</h3>
+            <hr className="mt-4 border-gray-400" />
 
+            <form onSubmit={handleSubmit(handleSendParcel)} className="mt-4">
+                {/* Parcel Type */}
+                <div className="space-x-3 mb-6">
+                    <label className="label">
+                        <input
+                            type="radio"
+                            {...register("parcelType")}
+                            value="document"
+                            className="radio radio-accent"
+                            defaultChecked
+                        />
+                        Document
+                    </label>
+
+                    <label className="label">
+                        <input
+                            type="radio"
+                            {...register("parcelType")}
+                            value="non-document"
+                            className="radio radio-accent"
+                        />
+                        Non-Document
+                    </label>
+                </div>
+
+                {/* Parcel Info */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 my-8">
+                    <fieldset className="fieldset">
+                        <label className="label text-xl text-primary font-bold">Parcel Name</label>
+                        <input
+                            type="text"
+                            {...register("parcelName")}
+                            className="input w-full"
+                            placeholder="Parcel Name"
+                        />
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <label className="label text-xl text-primary font-bold">Parcel Weight (KG)</label>
+                        <input
+                            type="number"
+                            {...register("parcelWeight")}
+                            className="input w-full"
+                            placeholder="Parcel Weight (KG)"
+                        />
+                    </fieldset>
+                </div>
+
+                <hr className="my-6 border-gray-300" />
+
+                {/* Sender & Receiver Details */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                    {/* Sender Details */}
+                    <fieldset className="fieldset">
+                        <h1 className="text-2xl font-semibold text-primary mb-6">Sender Details</h1>
+                        <label className="label text-[17px] text-primary font-bold">Sender Name</label>
+                        <input
+                            type="text"
+                            {...register("senderName")}
+                            className="input w-full"
+                            placeholder="Sender Name"
+                        />
+
+                        <label className="label text-[17px] text-primary font-bold">Sender Email</label>
+                        <input
+                            type="email"
+                            {...register("senderEmail")}
+                            className="input w-full"
+                            placeholder="Sender Email"
+                        />
+
+                        <label className="label text-[17px] text-primary font-bold">Sender Region</label>
+                        <select {...register("senderRegion")} className="select w-full">
+                            <option disabled selected>
+                                Pick a region
+                            </option>
+                            {
+                                regions.map((r, inx) => <option key={inx}>{r}</option>)
+                            }
+                        </select>
+
+                        <label className="label text-[17px] text-primary font-bold">Sender District</label>
+                        <select {...register("senderDistrict")} className="select w-full">
+                            <option disabled selected>
+                                Pick a district
+                            </option>
+                            {
+                                districtsByRegion(senderRegion).map((d, inx) => <option key={inx}>{d}</option>)
+                            }
+                        </select>
+
+                        <label className="label text-[17px] text-primary font-bold">Pickup Instructions</label>
+                        <textarea
+                            {...register("pickupInstructions")}
+                            className="textarea w-full"
+                            placeholder="Any special instructions"
+                        />
+                    </fieldset>
+
+                    {/* Receiver Details */}
+                    <fieldset className="fieldset">
+                        <h1 className="text-2xl font-semibold text-primary mb-6">Receiver Details</h1>
+                        <label className="label text-[17px] text-primary font-bold">Receiver Name</label>
+                        <input
+                            type="text"
+                            {...register("receiverName")}
+                            className="input w-full"
+                            placeholder="Receiver Name"
+                        />
+
+                        <label className="label text-[17px] text-primary font-bold">Receiver Email</label>
+                        <input
+                            type="email"
+                            {...register("receiverEmail")}
+                            className="input w-full"
+                            placeholder="Receiver Email"
+                        />
+
+                        <label className="label text-[17px] text-primary font-bold">Receiver Region</label>
+                        <select {...register("receiverRegion")} className="select w-full">
+                            <option disabled selected>
+                                Pick a region
+                            </option>
+                            {
+                                regions.map((r, inx) => <option key={inx}>{r}</option>)
+                            }
+                        </select>
+
+                        <label className="label text-[17px] text-primary font-bold">Receiver District</label>
+                        <select {...register("receiverDistrict")} className="select w-full">
+                            <option disabled selected>
+                                Pick a district
+                            </option>
+                            {
+                                districtsByRegion(receiverRegion).map((d, inx) => <option key={inx}>{d}</option>)
+                            }
+                        </select>
+
+                        <label className="label text-[17px] text-primary font-bold">Delivery Instructions</label>
+                        <textarea
+                            {...register("deliveryInstructions")}
+                            className="textarea w-full"
+                            placeholder="Any special instructions"
+                        />
+                    </fieldset>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-secondary text-primary mt-6 w-full md:w-auto"
+                >
+                    Proceed to Confirm Booking
+                </button>
+            </form>
         </div>
-    </div>;
+    );
 }
